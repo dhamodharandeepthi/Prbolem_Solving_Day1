1fff360d1b5b0c7e0c395023e53585c77cba2231
Dhamodharandeepthi <rdhamodharan22@gmail.com>
2024-05-16 12:28:37 +0530

    fix:optmized method1

--- sorting_an_array.js
+++ sorting_an_array.js
@@ -1,5 +1,6 @@
 // sorting an array
 // method1
+/*
 let arr = [9, 4, 5];
 console.log(arr.length); //3
 
@@ -18,3 +19,24 @@ for (let ind = 0; ind < arr.length; ind++) {
 }
 console.log(temp); //[ 4, 5, 9 ]
 console.log(arr); //[ Infinity, Infinity, Infinity ]
+*/
+//method2
+let arr = [9, 4, 5];
+console.log(arr.length); //3
+
+for (let ind = 0; ind < arr.length; ind++) {
+  let min = arr[ind];
+  let minInd = ind;
+  for (let ind1 = ind + 1; ind1 < arr.length; ind1++) {
+    if (min > arr[ind1]) {
+      min = arr[ind1];
+      minInd = ind1;
+    }
+  }
+  temp = arr[ind]; //5,4,9
+  arr[ind] = min;
+  arr[minInd] = temp; //firstloop => [4,9,5] second loop => [4,5,9]
+}
+
+console.log(arr); //[ 4, 5, 9 ]
+console.log(...arr); //4 5 9
