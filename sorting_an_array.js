560cb7a3445c84906fec3129b3a21469009134e9
Dhamodharandeepthi <rdhamodharan22@gmail.com>
2024-05-16 18:21:14 +0530

    feat:To find minimum element from an array

--- sorting_an_array.js
+++ sorting_an_array.js
@@ -38,6 +38,7 @@ for (let ind = 0; ind < arr.length; ind++) {
 }
 
 console.log(arr);
+console.log("Minium element from an array:", arr[0]);
 console.log(...arr);
 //Time complexity =O(n^2)  //[O(n)*O(n)=O(n^2)] // O(n)=>one loop
 //space complexcity:O(n)  //n=1 => O(1)
