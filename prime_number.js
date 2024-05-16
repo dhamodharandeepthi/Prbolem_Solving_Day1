5717d76f340515a5383022964e4d2b3bdcab7279
Dhamodharandeepthi <rdhamodharan22@gmail.com>
2024-05-16 16:08:20 +0530

    fix:found prime number if input is oddnumber

--- prime_number.js
+++ prime_number.js
@@ -12,5 +12,11 @@ function isPrime(num) {
   return true;
 }
 
-console.log("Given EvenNumber is prime number?: ", isPrime(100)); //false
-console.log("Number of Iteration: ", count); //Number of Iteration:  1
+console.log("Given EvenNumber is prime number?: ", isPrime(101)); //true
+console.log("Number of Iteration: ", count); //Number of Iteration:  99
+/*
+
+The time complexity of the provided isPrime function is O(n), where n is the input number. This is because, in the worst case, the function checks divisibility of the input number by all numbers from 2 up to the input number minus 1. that means (n-1)
+
+The space complexity of the function is O(1), because it uses a fixed amount of memory regardless of the input size. The space used by the function remains constant, as it doesn't depend on the input number.
+*/
